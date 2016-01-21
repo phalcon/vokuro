881d70f695d6a534fb0bbad831aff6e762d72d40
Serghei Iakovlev <sadhooklay@gmail.com>
2016-01-22 00:51:01 +0200

    :lipstick:

--- app/config/loader.php
+++ app/config/loader.php
@@ -1,15 +1,18 @@
 <?php
-$loader = new \Phalcon\Loader();
+
+use Phalcon\Loader;
+
+$loader = new Loader();
 
 /**
  * We're a registering a set of directories taken from the configuration file
  */
-$loader->registerNamespaces(array(
-    'Vokuro\Models' => $config->application->modelsDir,
+$loader->registerNamespaces([
+    'Vokuro\Models'      => $config->application->modelsDir,
     'Vokuro\Controllers' => $config->application->controllersDir,
-    'Vokuro\Forms' => $config->application->formsDir,
-    'Vokuro' => $config->application->libraryDir
-));
+    'Vokuro\Forms'       => $config->application->formsDir,
+    'Vokuro'             => $config->application->libraryDir
+]);
 
 $loader->register();
 
