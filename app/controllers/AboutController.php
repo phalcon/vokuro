9c56f45b1390422b3deea472ad19fa060a9d47d8
sergeysviridenko <sergey.v.sviridenko@gmail.com>
2017-08-14 18:49:35 +0300

    added link to user panel and logout on about page

--- app/controllers/AboutController.php
+++ app/controllers/AboutController.php
@@ -12,6 +12,7 @@ class AboutController extends ControllerBase
      */
     public function indexAction()
     {
+        $this->view->setVar('logged_in', is_array($this->auth->getIdentity()));
         $this->view->setTemplateBefore('public');
     }
 }
