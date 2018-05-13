0b844c4edc4ffc04b39876512118edb8039a155f
Mohsin Hasan <39223873+MoHasanJ@users.noreply.github.com>
2018-05-13 20:07:28 +0530

    Update IndexController.php

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -13,7 +13,6 @@ class IndexController extends ControllerBase
     public function indexAction()
     {
         $this->view->setVar('logged_in', is_array($this->auth->getIdentity()));
-        $this->view->setTemplateBefore('public');
-       
+        $this->view->setTemplateBefore('public');     
     }
 }
