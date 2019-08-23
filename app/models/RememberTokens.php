02d692e8a59cb8a42a53bfc41dcf99677b0ffe3d
Anton Vasiliev <aionbase@gmail.com>
2019-08-23 21:05:26 +0100

    #105 - Added license to header in .php files

--- app/models/RememberTokens.php
+++ app/models/RememberTokens.php
@@ -1,4 +1,14 @@
 <?php
+
+/**
+ * This file is part of the Vökuró.
+ *
+ * (c) Phalcon Team <team@phalconphp.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 namespace Vokuro\Models;
 
 use Phalcon\Mvc\Model;
