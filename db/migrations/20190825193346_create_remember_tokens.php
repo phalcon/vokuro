8643a81e2906cfd03db234164b3a2ebea743d35c
Anton Vasiliev <aionbase@gmail.com>
2019-08-25 20:58:37 +0100

    #101 - Added migration for 'remember_tokens'

---
+++ db/migrations/20190825193346_create_remember_tokens.php
@@ -0,0 +1,20 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateRememberTokens extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('remember_tokens');
+        if ($table->exists()) {
+            return;
+        }
+
+        $table->addColumn('usersId', 'integer')
+            ->addColumn('token', 'char', ['limit' => 32])
+            ->addColumn('userAgent', 'text')
+            ->addColumn('createdAt', 'integer')
+            ->addIndex(['token']);
+    }
+}
