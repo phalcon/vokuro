d4e6c21a9999112355522e0fcdaf9ef3908cf252
Anton Vasiliev <aionbase@gmail.com>
2019-08-27 22:03:51 +0100

    #107 - Added .htrouter.php for local PHP build-in web server

---
+++ .htrouter.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the Vökuró.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+    return false;
+}
+
+$_GET['_url'] = $_SERVER['REQUEST_URI'];
+
+require_once __DIR__ . '/public/index.php';
