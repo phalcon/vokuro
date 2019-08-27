a97d6455de44fb0f04401f55fb6fcc7db0d07f7f
Anton Vasiliev <aionbase@gmail.com>
2019-08-27 09:17:59 +0100

    #107 - Added more tests for SessionControllerCest class

--- tests/acceptance/controllers/SessionControllerCest.php
+++ tests/acceptance/controllers/SessionControllerCest.php
@@ -8,4 +8,34 @@ class SessionControllerCest
         $I->amOnPage('/session/login');
         $I->see('Log In');
     }
+
+    public function testSignup(\AcceptanceTester $I): void
+    {
+        $I->amOnPage('/session/signup');
+        $I->see('Sign up');
+    }
+
+    public function testForgotPassword(\AcceptanceTester $I): void
+    {
+        $I->amOnPage('/session/forgotPassword');
+        $I->see('Forgot Password?');
+    }
+
+    public function testLogoutAsGuest(\AcceptanceTester $I): void
+    {
+        $I->amOnPage('/session/logout');
+        $I->see('Welcome!');
+    }
+
+    public function testLogoutAsUser(\AcceptanceTester $I): void
+    {
+        $I->amOnPage('/session/login');
+        $I->see('Log In');
+        $I->fillField('email', 'bob@phalcon.io');
+        $I->fillField('password', 'password1');
+        $I->click('//form/*[@type="submit"]');
+        $I->see('Search users');
+        $I->amOnPage('/session/logout');
+        $I->see('Welcome!');
+    }
 }
