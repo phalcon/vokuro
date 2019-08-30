19df446c517793431cb75c4b6920e9619af71b37
Anton Vasiliev <aionbase@gmail.com>
2019-08-30 23:40:40 +0100

    #107 - Updated array list of providers

--- configs/providers.php
+++ configs/providers.php
@@ -1,5 +1,19 @@
 <?php
 
 return [
+    \Vokuro\Providers\AclProvider::class,
+    \Vokuro\Providers\AuthProvider::class,
     \Vokuro\Providers\ConfigProvider::class,
+    \Vokuro\Providers\CryptProvider::class,
+    \Vokuro\Providers\DbProvider::class,
+    \Vokuro\Providers\DispatcherProvider::class,
+    \Vokuro\Providers\FlashProvider::class,
+    \Vokuro\Providers\LoggerProvider::class,
+    \Vokuro\Providers\MailProvider::class,
+    \Vokuro\Providers\ModelsMetadataProvider::class,
+    \Vokuro\Providers\RouterProvider::class,
+    \Vokuro\Providers\SessionBagProvider::class,
+    \Vokuro\Providers\SessionProvider::class,
+    \Vokuro\Providers\UrlProvider::class,
+    \Vokuro\Providers\ViewProvider::class,
 ];
