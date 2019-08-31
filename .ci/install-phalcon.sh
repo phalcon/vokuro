b4961232f0abc7fd72029356fd1862460cfa2f88
Anton Vasiliev <aionbase@gmail.com>
2019-08-31 22:08:42 +0100

    #107 - Change Phalcon installation through compilation

--- .ci/install-phalcon.sh
+++ .ci/install-phalcon.sh
@@ -7,19 +7,8 @@
 # For the full copyright and license information, please view the
 # LICENSE.txt file that was distributed with this source code.
 
-PHP_VERSION="$(php -r 'echo phpversion();' | cut -d '.' -f 1,2)"
 PHALCON_VERSION=4.0.0-beta.2
-PHALCON_BUILD=754
-PHALCON_BRANCH=mainline
-PHALCON_OS=ubuntu/xenial
 
-PHALCON_REPO="https://packagecloud.io/phalcon/$PHALCON_BRANCH"
-PHALCON_PKG="php$PHP_VERSION-phalcon_$PHALCON_VERSION-$PHALCON_BUILD+php${PHP_VERSION}_amd64.deb"
-
-curl -sSL \
-  "$PHALCON_REPO/packages/$PHALCON_OS/$PHALCON_PKG/download.deb" \
-  -o /tmp/phalcon.deb \
-
-mkdir /tmp/pkg
-dpkg-deb -R /tmp/phalcon.deb /tmp/pkg
-cp /tmp/pkg/usr/lib/php/*/phalcon.so "$(php-config  --extension-dir)/phalcon.so"
+git clone --depth=1 -v https://github.com/phalcon/cphalcon.git -b ${PHALCON_VERSION} /tmp/phalcon
+cd /tmp/phalcon/build
+./install --phpize $(phpenv which phpize) --php-config $(phpenv which php-config)
