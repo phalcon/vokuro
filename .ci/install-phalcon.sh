7a0a9bbc6683f80f83a8e4a9581173cc3a1bd660
Anton Vasiliev <aionbase@gmail.com>
2019-08-31 22:12:50 +0100

    #107 - Fix PHALCON_VERSION value

--- .ci/install-phalcon.sh
+++ .ci/install-phalcon.sh
@@ -7,7 +7,7 @@
 # For the full copyright and license information, please view the
 # LICENSE.txt file that was distributed with this source code.
 
-PHALCON_VERSION=4.0.0-beta.2
+PHALCON_VERSION=v4.0.0-beta.2
 
 git clone --depth=1 -v https://github.com/phalcon/cphalcon.git -b ${PHALCON_VERSION} /tmp/phalcon
 cd /tmp/phalcon/build
