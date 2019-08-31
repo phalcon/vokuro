47e1f4d6a6067cc56b43c80ed97d3e4cda795e8f
Anton Vasiliev <aionbase@gmail.com>
2019-08-31 15:40:38 +0100

    #107 - Added copyright docs

--- configs/providers.php
+++ configs/providers.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Vökuró.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 return [
     \Vokuro\Providers\AclProvider::class,
     \Vokuro\Providers\AuthProvider::class,
