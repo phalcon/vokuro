d52a0fdac8b295847e9adba8acb442d1762fda45
Anton Vasiliev <aionbase@gmail.com>
2019-08-31 22:27:31 +0100

    #107 - Move out PHALCON_VERSION variable from install-phalcon.sh

--- .ci/install-phalcon.sh
+++ .ci/install-phalcon.sh
@@ -7,8 +7,6 @@
 # For the full copyright and license information, please view the
 # LICENSE.txt file that was distributed with this source code.
 
-PHALCON_VERSION=v4.0.0-beta.2
-
-git clone --depth=1 -v https://github.com/phalcon/cphalcon.git -b ${PHALCON_VERSION} /tmp/phalcon
+git clone --depth=1 -v https://github.com/phalcon/cphalcon.git -b ${PHALCON_VERSION:=master} /tmp/phalcon
 cd /tmp/phalcon/build
 ./install --phpize $(phpenv which phpize) --php-config $(phpenv which php-config)
