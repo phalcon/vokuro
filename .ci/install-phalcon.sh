de6601a6d28678e5abe05034286609fe589ec1b6
Serghei Iakovlev <sadhooklay@gmail.com>
2019-09-03 19:34:48 +0300

    Unify Travis CI configuration

--- .ci/install-phalcon.sh
+++ .ci/install-phalcon.sh
@@ -7,6 +7,14 @@
 # For the full copyright and license information, please view the
 # LICENSE.txt file that was distributed with this source code.
 
-git clone --depth=1 -v https://github.com/phalcon/cphalcon.git -b ${PHALCON_VERSION:=master} /tmp/phalcon
-cd /tmp/phalcon/build
-./install --phpize $(phpenv which phpize) --php-config $(phpenv which php-config)
+set -e
+
+: "${PHALCON_VERSION:=master}"
+
+git clone --depth=1 -v https://github.com/phalcon/cphalcon.git -b "$PHALCON_VERSION" /tmp/phalcon
+cd /tmp/phalcon/build || extit 1
+
+./install --phpize "$(phpenv which phpize)" --php-config "$(phpenv which php-config)" 1> /dev/null
+echo extension=phalcon.so >> "$(phpenv prefix)/etc/php.ini"
+
+"$(phpenv which php)" -m | grep -q phalcon
