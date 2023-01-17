eedd01c32a0a21727461ce05c961c22f842fbbbf
Sea man <31950816+shenyman@users.noreply.github.com>
2023-01-17 20:31:14 +0200

    Update LoginForm.php

--- src/Forms/LoginForm.php
+++ src/Forms/LoginForm.php
@@ -18,9 +18,9 @@ use Phalcon\Forms\Element\Password;
 use Phalcon\Forms\Element\Submit;
 use Phalcon\Forms\Element\Text;
 use Phalcon\Forms\Form;
-use Phalcon\Validation\Validator\Email;
-use Phalcon\Validation\Validator\Identical;
-use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Filter\Validation\Validator\Email;
+use Phalcon\Filter\Validation\Validator\Identical;
+use Phalcon\Filter\Validation\Validator\PresenceOf;
 
 class LoginForm extends Form
 {
